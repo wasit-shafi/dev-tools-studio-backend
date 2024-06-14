61835e2d4e24b7cb72b775b628d961ce7112a7dd
wasit-shafi <wasitshafi700@gmail.com>
2024-06-15 04:05:14 +0530

    nodemon, ts-node with build in --env-file setup done

---
+++ src/config/config.ts
@@ -0,0 +1,20 @@
+const _config: Record<string, string | number | undefined> = {
+	API_KEY: process.env.apiKey,
+	PORT: process.env.PORT,
+};
+
+export const config = {
+	get(key: string): string | number | undefined {
+		if (!key || typeof key !== 'string') {
+			throw new Error(
+				"Key should be of type 'string'(length >= 0) for accessing environment variables"
+			);
+		}
+
+		if (!Object.keys(_config).includes(key)) {
+			throw new Error('Invalid key for accessing environment variables');
+		}
+
+		return _config[key];
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import { config } from './config/config';
+
 import express from 'express';
+
+const port = config.get('PORT');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.json({ message: `Server is running on port ${port}` }).send();
 });
 
 app.listen(port, () => {

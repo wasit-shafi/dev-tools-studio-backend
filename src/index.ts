9fcd94a91b86a21a816946a0d4d5b05b7d5465b3
wasit-shafi <wasitshafi700@gmail.com>
2025-06-05 09:09:34 +0530

    removed extra indentation + add log for testing/validating env injection using github actions

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ connectDatabase()
 		const port = _env.get('EXPRESS_PORT');
 		app.listen({ port }, () => {
 			logger.info(`Server is running on port ${port}`);
+			// Temp for testing/validating env via github actions
+
+			for (const key in process.env) {
+				console.log('[' + key + '->' + process.env[key] + '->' + typeof process.env[key] + ']');
+			}
 		});
 	})
 	.catch((error) => {

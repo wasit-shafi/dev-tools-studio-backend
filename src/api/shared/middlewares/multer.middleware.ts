ddb13c2b24332dc1fa6fc9beaef9102710ec395f
wasit-shafi <wasitshafi700@gmail.com>
2025-07-09 16:12:53 +0530

    updated error message for multer middleware

--- src/api/shared/middlewares/multer.middleware.ts
+++ src/api/shared/middlewares/multer.middleware.ts
@@ -36,7 +36,7 @@ export const upload = multer({
 	fileFilter: (request: Request, file: any, callback) => {
 		// TODO(Wasit): Remove below line when AWS S3 cloud storage is enabled back.
 
-		callback(new Error('Uploading/Storing files on cloud storage (AWS S3 Bucket) is temporarily disabled since 26 July 2025.'));
+		callback(new Error('Uploading/Storing files on cloud storage (AWS S3 Bucket) is temporarily disabled since 26 June 2025.'));
 
 		const { path = '' } = request;
 
